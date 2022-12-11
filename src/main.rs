ac7fc35a4774c602e7e31122cbb2db7d5f100169
Shen Li <dustet@gmail.com>
2022-12-10 23:09:41 -0500

    hard code a pong response

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 // Uncomment this block to pass the first stage
 // use std::net::TcpListener;
 
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,10 +13,17 @@ fn main() {
     // Uncomment this block to pass the first stage
     //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
+                let mut buf: [u8; 1024] = [0; 1024];
+                match stream.read(&mut buf) {
+                    Ok(_size) => {
+                        stream.write(&"+PONG\r\n".as_bytes()).unwrap();
+                    }
+                    Err(_) => {}
+                }
                 println!("accepted new connection");
             }
             Err(e) => {
